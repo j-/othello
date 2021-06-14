242662ea6b5ebcc954a0f0a075ec129e3ba63c13
Jamie Hoeks <jamie.hoeks@team.belong.com.au>
2021-06-14 14:03:04 +1000

    Add simple game difficulties

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -6,29 +6,59 @@ import { getBot } from './worker-bot';
 import OthelloBoard from './OthelloBoard';
 import './App.css';
 
-const App: React.FC = () => {
-  const [iterations] = React.useState(500);
-  const [playoutDepth] = React.useState(59);
+export interface BotDifficulty {
+  name: string;
+  iterations: number;
+  playoutDepth: number;
+}
 
-  const OthelloClient = React.useMemo(() => (
-    Client({
+export const BOT_EASY = { name: 'Easy', iterations: 5, playoutDepth: 5 };
+export const BOT_MEDIUM = { name: 'Medium', iterations: 100, playoutDepth: 15 };
+export const BOT_HARD = { name: 'Hard', iterations: 500, playoutDepth: 59 };
+export const BOT_EXPERT = { name: 'Expert', iterations: 2000, playoutDepth: 59 };
+
+export const BOT_DIFFICULTIES: BotDifficulty[] = [
+  BOT_EASY,
+  BOT_MEDIUM,
+  BOT_HARD,
+  BOT_EXPERT,
+];
+
+const App: React.FC = () => {
+  const [isGameStarted, setIsGameStarted] = React.useState(false);
+  const [gameNumber, setGameNumber] = React.useState(1);
+  const [difficulty, setDifficulty] = React.useState(BOT_MEDIUM);
+
+  const OthelloClient = React.useMemo(() => {
+    // Must consume game number to recompute memo
+    console.info('Game number ' + gameNumber);
+    return Client({
       debug: false,
       game: OthelloGame,
       board: OthelloBoard,
       multiplayer: Local({
         bots: {
-          1: getBot({
-            iterations,
-            playoutDepth,
-          }),
+          1: getBot(difficulty),
         },
       }),
-    })
-  ), [iterations, playoutDepth]);
+    });
+  }, [gameNumber, difficulty]);
+
+  const reset = React.useCallback(() => {
+    setGameNumber(gameNumber + 1);
+  }, [gameNumber]);
 
   return (
     <div className="App">
-      <OthelloClient playerID="0" />
+      {isGameStarted && <OthelloClient playerID="0" />}
+      {isGameStarted && <button type="button" onClick={reset}>New game</button>}
+      {BOT_DIFFICULTIES.map((difficulty) => (
+        <button key={difficulty.name} type="button" onClick={() => {
+          setIsGameStarted(true);
+          setGameNumber(gameNumber + 1);
+          setDifficulty(difficulty);
+        }}>{difficulty.name} game</button>
+      ))}
     </div>
   );
 };
